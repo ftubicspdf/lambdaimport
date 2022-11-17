bb0f0163a89eef059ca4b82e52679e47b0a835d5
Filip Tubic <filip.tubic@smallpdf.com>
2022-11-17 11:23:03 +0100

    remove caching

--- main.go
+++ main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/kelseyhightower/envconfig"
-	"io"
 	"net/http"
-	"os"
 )
 
 var (
@@ -45,24 +42,6 @@ func download(url string, headers map[string]string) (response *http.Response, e
 	return client.Do(req)
 }
 
-func cache(src io.Reader) (*os.File, error) {
-	fp, err := os.CreateTemp("", "")
-	if err != nil {
-		return nil, err
-	}
-
-	_, err = io.Copy(fp, src)
-	if err != nil {
-		return nil, err
-	}
-	_, err = fp.Seek(0, 0)
-	if err != nil {
-		return nil, err
-	}
-
-	return fp, nil
-}
-
 func HandleRequest(ctx context.Context, event ImportEvent) (string, error) {
 	sess := session.Must(session.NewSession())
 
@@ -70,23 +49,6 @@ func HandleRequest(ctx context.Context, event ImportEvent) (string, error) {
 	if err != nil {
 		return "", err
 	}
-
-	fp, err := cache(response.Body)
-	if err != nil {
-		return "", err
-	}
-	defer func(fp *os.File) {
-		err := fp.Close()
-		if err != nil {
-			fmt.Printf("failed to close the file %s: %v\n", event.FileName, err)
-		}
-	}(fp)
-	defer func(name string) {
-		err := os.Remove(name)
-		if err != nil {
-			fmt.Printf("failed to delete file %s: %v\n", name, err)
-		}
-	}(fp.Name())
 
 	uploader := s3manager.NewUploader(sess, func(u *s3manager.Uploader) {
 		u.PartSize = 5 * 1024 * 1024 // The minimum/default allowed part size is 5MB
@@ -98,7 +60,7 @@ func HandleRequest(ctx context.Context, event ImportEvent) (string, error) {
 		&s3manager.UploadInput{
 			Bucket: aws.String(cfg.S3FileBucket),
 			Key:    aws.String(event.FileName),
-			Body:   fp,
+			Body:   response.Body,
 		},
 	)
 	if err != nil {
